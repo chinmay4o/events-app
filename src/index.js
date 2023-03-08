9298a43e7d1339eea87fb828ccfd204252eb0330
chinmay4o <surve4404@gmail.com>
2023-03-08 17:37:28 +0530

    added error boundries

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
+import ErrorBoundary from "./ErrorBoundary";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Router>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Router>
+  <React.StrictMode>
+    <Router>
+      <ErrorBoundary>
+        <App />
+      </ErrorBoundary>
+    </Router>
+  </React.StrictMode>
 );
