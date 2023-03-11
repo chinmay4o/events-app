18dd1544704fe71e48b69182b57b6dc84d4de71b
chinmay4o <surve4404@gmail.com>
2023-03-11 18:58:15 +0530

    Fixed CSS issues

--- src/components/eventLayout/communications/marketing/Marketing.jsx
+++ src/components/eventLayout/communications/marketing/Marketing.jsx
@@ -20,7 +20,7 @@ function Marketing() {
             )
           }
         >
-          <div className="flex items-center">
+          <div className="flex items-center cursor-pointer">
             <svg
               aria-hidden="true"
               className="flex-shrink-0 w-6 h-6 m-2 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
@@ -31,6 +31,28 @@ function Marketing() {
               <path d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"></path>
             </svg>
             <p>Email marketing</p>
+          </div>
+          <p className="p-3 text-gray-500 text-l">{">"}</p>
+        </div>
+        <div
+          className="flex items-center justify-between pb-2 border-b-2 cursor-pointer"
+          onClick={() => {
+            navigate(
+              `/events/${eventsId.params.eventId}/communications/linkedin-marketing`
+            );
+          }}
+        >
+          <div className="flex items-center">
+            <svg
+              aria-hidden="true"
+              className="flex-shrink-0 w-6 h-6 m-2 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"></path>
+            </svg>
+            <p>LinkedIn marketing </p>
           </div>
           <p className="p-3 text-gray-500 text-l">{">"}</p>
         </div>
@@ -61,28 +83,6 @@ function Marketing() {
               <path d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"></path>
             </svg>
             <p>SMS marketing </p>
-          </div>
-          <p className="p-3 text-gray-500 text-l">{">"}</p>
-        </div>
-        <div
-          className="flex items-center justify-between pb-2 border-b-2 cursor-pointer"
-          onClick={() => {
-            navigate(
-              `/events/${eventsId.params.eventId}/communications/linkedin-marketing`
-            );
-          }}
-        >
-          <div className="flex items-center">
-            <svg
-              aria-hidden="true"
-              className="flex-shrink-0 w-6 h-6 m-2 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"></path>
-            </svg>
-            <p>LinkedIn marketing </p>
           </div>
           <p className="p-3 text-gray-500 text-l">{">"}</p>
         </div>
